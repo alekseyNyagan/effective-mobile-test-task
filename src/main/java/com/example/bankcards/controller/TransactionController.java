f951a49cf2789257d0988eb82f511092965e4be6
aleksey <alexei.muraschkin@gmail.com>
2025-11-18 11:37:33 +0500

    make TransactionService in TransactionController private final to Lombok correctly generating the constructor for dependency injection

--- src/main/java/com/example/bankcards/controller/TransactionController.java
+++ src/main/java/com/example/bankcards/controller/TransactionController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/transfer")
 public class TransactionController {
 
-    TransactionService transactionService;
+    private final TransactionService transactionService;
 
     @PostMapping("/transfer")
     public ResponseEntity<Void> transfer(@Valid @RequestBody TransferRequest request) {
